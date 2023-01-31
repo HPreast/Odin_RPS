b4c747c7eb783090a6708ff73ac1ae8432f6c790
HPreast <preasths1@gmail.com>
2023-01-30 20:20:37 -0600

    Finished writing logic for playRound function, starting logic to track scores to show winner

--- rps.js
+++ rps.js
@@ -1,11 +1,35 @@
-let choices = ['Rock', 'Paper', 'Scissors'];
+let choices = ['ROCK', 'PAPER', 'SCISSORS'];
 
 function getComputerChoice(arr) {
     const randomIndex = Math.floor(Math.random() * arr.length);
     return arr[randomIndex];
 }
-console.log(getComputerChoice(choices));
 
 function playRound(playerSelection, computerSelection) {
-    return
+    if (computerSelection == playerSelection.toUpperCase()) {
+        return 'Draw!';
+    } else if (playerSelection.toUpperCase() == choices[0]) {
+        if (computerSelection == choices[1]) {
+            return `You Lose, ${computerSelection} beats ${playerSelection}!`;
+        } else if (computerSelection == choices[2]) {
+            return `You Win! ${playerSelection} beats ${computerSelection}!`;
+        }
+    } else if (playerSelection.toUpperCase() == choices[1]) {
+        if (computerSelection == choices[2]) {
+            return `You Lose, ${computerSelection} beats ${playerSelection}!`;
+        } else if (computerSelection == choices[0]) {
+            return `You Win! ${playerSelection} beats ${computerSelection}!`;
+        }
+    } else if (playerSelection.toUpperCase() == choices[2]) {
+        if (computerSelection == choices[0]) {
+            return `You Lose, ${computerSelection} beats ${playerSelection}!`;
+        } else if (computerSelection == choices[1]) {
+            return `You Win! ${playerSelection} beats ${computerSelection}!`;
+        }
+    }
 }
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice(choices);
+console.log(getComputerChoice(choices));
+console.log(playRound(playerSelection, computerSelection));
